4ba48a53588a7c4c24f27d1632c92fd472574fef
Javier Dominguez Galindo <joshjavy@gmail.com>
2024-01-19 16:10:54 -0600

    75. Primeros pasos con el Formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -12,6 +12,28 @@
         <p class="text-lg mt-5 text-center mb-10">
             Añade pacientes y
             <span class="text-indigo-600 font-bold">Adminístralos</span>
+            <form
+                class="bg-white shadow-md rounded-lg py-10 px-5 md-10"
+            >
+                <div
+                    class="mb-5"
+                >
+                    <label
+                        class="block text-gray-700 uppercase font-bold"
+                        for="mascota">
+                        Nombre Mascota
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="Nombre de la mascota"
+                        id="mascota"
+                        name="mascota"
+                        class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    />
+                    
+                </div>
+            </form>
+
         </p>
     </div>
 </template>
