324f6c323ab0e7bf4270e1b3c997fb047deb8a3d
Javier Dominguez Galindo <joshjavy@gmail.com>
2024-01-19 15:58:45 -0600

    74. Creando el Componente para el Formulario

---
+++ src/components/Formulario.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="w-1/2 md:w/2">
+        <h2 
+            class="font-black text-3xl text-center"
+        >
+            Seguimientpo Paciente
+        </h2>
+        <p class="text-lg mt-5 text-center mb-10">
+            Añade pacientes y
+            <span class="text-indigo-600 font-bold">Adminístralos</span>
+        </p>
+    </div>
+</template>
